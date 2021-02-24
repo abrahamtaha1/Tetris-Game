a2ec98f7bb7664b3f61fecbec76e5ededca582b9
tahaa8 <tahaa8@mcmaster.ca>
2021-02-24 00:07:08 -0400

    added a movedown function and a freezing block function for the bottom of the grid

--- src/TetrisGame.js
+++ src/TetrisGame.js
@@ -94,5 +94,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('keyup', control)
    
+    function moveDown(){
+        eraseBlock()
+        currentPosition += width
+        draw()
+        freezeBlock()
+    }
+
+    //stop the blocks at the bottom and make new block
+    function freezeBlock(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            random = nextRandom
+            nextRandom = Math.floor(Math.random()*Blocks.length)
+            current = Blocks[random][currentRotation]
+            currentPosition = 4
+            draw()
+            displayShape()
+            addScore()
+            gameover()
+        }
+    }
 
 })
