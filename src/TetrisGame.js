e910e4f694a6a21e7dedf6dfc4535a3f8669100f
ali-tab <justalitabar@gmail.com>
2021-02-23 23:56:01 -0500

    Added addScore function

--- src/TetrisGame.js
+++ src/TetrisGame.js
@@ -202,4 +202,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    //filling a row of blocks and adding to the score
+
+    function addScore() {
+        for(let i = 0; i < 199; i += width){
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+            if(row.every(index => squares[index].classList.contains('taken'))){
+                score += 10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('blocks')
+                    squares[index].style.backgroundColor = ''
+                })
+                const squaresRemoved = squares.splice(i, width) 
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
+
 })
